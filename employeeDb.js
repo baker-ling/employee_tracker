adadbfebb5f29bb5af909d553c79e76d388fbe1b
Brian D. Baker <brian.devs.baker@gmail.com>
2021-10-17 21:02:06 -0400

    Added jsdocs to all functions

--- employeeDb.js
+++ employeeDb.js
@@ -5,11 +5,31 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 /**
- * @typedef {Object} roleData
+ * @typedef {Object} DepartmentData
+ * @property {number} [id]
+ * @property {name} name
+ */
+
+/**
+ * @typedef {Object} RoleData
  * @property {number} [id]
  * @property {string} title
  * @property {number} salary
  * @property {number} department_id
+ * @property {string} [department]
+ */
+
+/**
+ * @typedef {Object} EmployeeData
+ * @property {number} [id]
+ * @property {string} first_name
+ * @property {string} last_name
+ * @property {number} role_id
+ * @property {string} [title]
+ * @property {string|number} [salary]
+ * @property {string} [department]
+ * @property {?number} [manager_id]
+ * @property {?string} [manager]
  */
 
 /**
@@ -20,6 +40,10 @@ const employeeDb = {
   
   connection: null,
 
+  /**
+   * Initializes the connection to the database.
+   * Must be run before any other methods.
+   */
   async initializeConnection () {
     this.connection = await mysql.createConnection({
       host: 'localhost', 
@@ -27,18 +51,28 @@ const employeeDb = {
       password: process.env.DB_PASSWORD,
       database: 'employee_db'
     });
-    return this;
   },
 
+  /**
+   * Closes the connection to the database.
+   */
   async closeConnection() {
     this.connection && this.connection.end();
   },
 
+  /**
+   * Gets all the departments in the database
+   * @returns {DepartmentData[]}
+   */
   async getDepartments() {
     const [departments] = await this.connection.query('SELECT * FROM department ORDER BY id');
     return departments;
   },
 
+  /**
+   * Gets all the roles in the database, including their departments
+   * @returns {RoleData[]}
+   */
   async getRoles() {
     const [roles] = await this.connection.query(
       `SELECT role.id AS id, title, department.name as department, salary
@@ -48,6 +82,10 @@ const employeeDb = {
     return roles;
   },
 
+  /**
+   * Gets all the employees in the database, including departmetns, titles, salaries, and managers (if assigned)
+   * @returns {EmployeeData[]}
+   */
   async getEmployees() {
     const [employees] = await this.connection.query(
       `SELECT emp.id, emp.first_name, emp.last_name, role.title, department.name AS department, role.salary, COALESCE(CONCAT(mgr.first_name, ' ', mgr.last_name), "") AS manager
@@ -63,7 +101,7 @@ const employeeDb = {
   /**
    * Gets all roles that a given department has
    * @param {number} department_id 
-   * @returns 
+   * @returns {RoleData[]} 
    */
   async getRolesByDepartmentId(department_id) {
     const [roles] = await this.connection.query(
@@ -76,7 +114,7 @@ const employeeDb = {
   /**
    * Gets all employee records that belong a department
    * @param {number} department_id 
-   * @returns 
+   * @returns {EmployeeData[]}
    */
   async getEmployeesByDepartmentId(department_id) {
     const [employees] = await this.connection.query(
@@ -94,7 +132,7 @@ const employeeDb = {
   /**
    * Adds a department to the database and returns the id of the inserted record
    * @param {string} departmentName 
-   * @returns
+   * @returns {number}
    */
   async addDepartment(departmentName) {
     const [result] = await this.connection.query(
@@ -106,7 +144,7 @@ const employeeDb = {
   /**
    * Adds a new role to the employee database.
    * @param {RoleData} roleData 
-   * @returns 
+   * @returns {number} 
    */
   async addRole(roleData) {
     const {title, salary, department_id} = roleData;
@@ -117,6 +155,11 @@ const employeeDb = {
     return result.insertId;
   },
 
+  /**
+   * Adds an employee to the database
+   * @param {EmployeeData} employeeData 
+   * @returns {number}
+   */
   async addEmployee(employeeData) {
     const {first_name, last_name, role_id, manager_id} = employeeData;
     const [result] = await this.connection.query(
@@ -126,6 +169,12 @@ const employeeDb = {
     return result.insertId;
   },
 
+  /**
+   * Updates the role id for the employee specified by id
+   * @param {number} employeeId 
+   * @param {number} newRoleId 
+   * @returns {number}
+   */
   async updateEmployeeRole(employeeId, newRoleId) {
     const [result] = await this.connection.query(
       `UPDATE employee SET role_id = ? WHERE id = ?`,
